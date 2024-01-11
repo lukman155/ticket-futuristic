9af5e00dad30d15ca2f01ee6752bc57cccb849f7
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2024-01-11 20:58:56 +0100

    Add - About Page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,190 @@
 <main>
-  <nav>
-    <div class="item one">
-        <a href="/"><img src="home-2.svg" alt="home"></a>
-        <a href="/"><img src="story.svg" alt="home"></a>
-    </div>
-    
-    <div class="item-logo">
-      <a href="/">
-      <div class="logo-con">
-        <svg width="80" height="17" viewBox="0 0 257 55" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M204.881 54.2286L224.397 26.3815L205.807 0H218.226L231.031 18.8219L243.528 0H255.407L237.434 26.0729L256.333 54.2286H244.068L230.723 33.5554L216.76 54.2286H204.881Z" fill="black"/>
-          <path d="M176.249 39.1092V29.9297H202.939V39.1092H176.249Z" fill="black"/>
-          <path d="M148.335 55C137.073 55 128.356 46.5147 128.356 35.4839C128.356 24.6073 136.996 16.1221 148.258 16.1221C159.597 16.1221 168.237 24.5302 168.237 35.4839C168.237 46.5147 159.597 55 148.335 55ZM148.258 45.5891C153.966 45.5891 157.9 41.4236 157.9 35.561C157.9 29.6985 153.889 25.6101 148.258 25.6101C142.627 25.6101 138.693 29.7756 138.693 35.561C138.693 41.4236 142.627 45.5891 148.258 45.5891Z" fill="black"/>
-          <path d="M103.612 54.2287L87.1819 16.9707H98.2899L107.315 40.498L116.495 16.9707H127.217L110.863 54.2287H103.612Z" fill="black"/>
-          <path d="M61.7346 55C51.5523 55 43.8384 46.669 43.8384 35.6382C43.8384 24.6073 51.5523 16.1992 61.6574 16.1992C66.1315 16.1992 69.9113 17.8191 72.534 20.519V16.9706H82.562V54.2286H72.534V50.6803C69.9113 53.3801 66.1315 55 61.7346 55ZM63.5859 45.6662C69.217 45.6662 73.074 41.5007 73.074 35.5611C73.074 29.6985 69.217 25.533 63.5859 25.533C58.0319 25.533 54.175 29.6985 54.175 35.5611C54.175 41.5007 58.0319 45.6662 63.5859 45.6662Z" fill="black"/>
-          <path d="M0 54.2286V0H21.2132C31.704 0 38.8008 6.63393 38.8008 15.8134C38.8008 24.3759 33.0925 30.2384 24.2216 31.3955L42.3492 54.2286H29.6213L12.2651 31.5498H10.4909V54.2286H0ZM20.2875 8.5624H10.4909V23.373H20.2875C25.4558 23.373 28.3871 20.5961 28.3871 15.9677C28.3871 11.8022 25.4558 8.5624 20.2875 8.5624Z" fill="black"/>
-        </svg>
+  <img class="about-bg" src="about-bg.png" alt="about">
+  <section>
+    <nav>
+      <div class="item one">
+          <a href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M9.02 2.84016L3.63 7.04016C2.73 7.74016 2 9.23016 2 10.3602V17.7702C2 20.0902 3.89 21.9902 6.21 21.9902H17.79C20.11 21.9902 22 20.0902 22 17.7802V10.5002C22 9.29016 21.19 7.74016 20.2 7.05016L14.02 2.72016C12.62 1.74016 10.37 1.79016 9.02 2.84016Z" stroke="#999DBA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M12 17.9902V14.9902" stroke="#999DBA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </a>
+          <a href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M16.42 7.95012C18.86 10.3901 18.86 14.3501 16.42 16.7901C13.98 19.2301 10.02 19.2301 7.58 16.7901C5.14 14.3501 5.14 10.3901 7.58 7.95012C10.02 5.51012 13.98 5.51012 16.42 7.95012Z" stroke="#999DBA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M8.24999 21.6399C6.24999 20.8399 4.49999 19.3899 3.33999 17.3799C2.19999 15.4099 1.81999 13.2199 2.08999 11.1299" stroke="#999DBA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M5.8501 4.47986C7.5501 3.14986 9.68009 2.35986 12.0001 2.35986C14.2701 2.35986 16.3601 3.12985 18.0401 4.40985" stroke="#999DBA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M15.75 21.6399C17.75 20.8399 19.5 19.3899 20.66 17.3799C21.8 15.4099 22.18 13.2199 21.91 11.1299" stroke="#999DBA" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </a>
       </div>
-      </a>
+      
+      <div class="item-logo">
+        <a href="/">
+          <div class="logo-con">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M18.0698 9.57L11.9998 3.5L5.92982 9.57" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M12 20.4999L12 3.66992" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <svg width="80" height="17" viewBox="0 0 257 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M204.881 54.2286L224.397 26.3815L205.807 0H218.226L231.031 18.8219L243.528 0H255.407L237.434 26.0729L256.333 54.2286H244.068L230.723 33.5554L216.76 54.2286H204.881Z" fill="white"/>
+              <path d="M176.249 39.1092V29.9297H202.939V39.1092H176.249Z" fill="white"/>
+              <path d="M148.335 55C137.073 55 128.356 46.5147 128.356 35.4839C128.356 24.6073 136.996 16.1221 148.258 16.1221C159.597 16.1221 168.237 24.5302 168.237 35.4839C168.237 46.5147 159.597 55 148.335 55ZM148.258 45.5891C153.966 45.5891 157.9 41.4236 157.9 35.561C157.9 29.6985 153.889 25.6101 148.258 25.6101C142.627 25.6101 138.693 29.7756 138.693 35.561C138.693 41.4236 142.627 45.5891 148.258 45.5891Z" fill="white"/>
+              <path d="M103.612 54.2287L87.1819 16.9707H98.2899L107.315 40.498L116.495 16.9707H127.217L110.863 54.2287H103.612Z" fill="white"/>
+              <path d="M61.7346 55C51.5523 55 43.8384 46.669 43.8384 35.6382C43.8384 24.6073 51.5523 16.1992 61.6574 16.1992C66.1315 16.1992 69.9113 17.8191 72.534 20.519V16.9706H82.562V54.2286H72.534V50.6803C69.9113 53.3801 66.1315 55 61.7346 55ZM63.5859 45.6662C69.217 45.6662 73.074 41.5007 73.074 35.5611C73.074 29.6985 69.217 25.533 63.5859 25.533C58.0319 25.533 54.175 29.6985 54.175 35.5611C54.175 41.5007 58.0319 45.6662 63.5859 45.6662Z" fill="white"/>
+              <path d="M0 54.2286V0H21.2132C31.704 0 38.8008 6.63393 38.8008 15.8134C38.8008 24.3759 33.0925 30.2384 24.2216 31.3955L42.3492 54.2286H29.6213L12.2651 31.5498H10.4909V54.2286H0ZM20.2875 8.5624H10.4909V23.373H20.2875C25.4558 23.373 28.3871 20.5961 28.3871 15.9677C28.3871 11.8022 25.4558 8.5624 20.2875 8.5624Z" fill="white"/>
+            </svg>
+          </div>
+        </a>
+      </div>
+    </nav>
+
+    <div class="about">
+      <h2>About</h2>
+      <p class="para">
+        Evgeniy Chebatkov is a Kazakh and Russian stand-up comedian, actor and podcaster. Chebatkov is a comedian with his own comedic voice, original themes and recognizable style. You might have seen his concerts
+      </p>
+
+      <div class="dates">
+        <p><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M8 2V5" stroke="#4F5266" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M16 2V5" stroke="#4F5266" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M3.5 9.08984H20.5" stroke="#4F5266" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M21 8.5V17C21 20 19.5 22 16 22H8C4.5 22 3 20 3 17V8.5C3 5.5 4.5 3.5 8 3.5H16C19.5 3.5 21 5.5 21 8.5Z" stroke="#4F5266" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M15.6947 13.7002H15.7037" stroke="#4F5266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M15.6947 16.7002H15.7037" stroke="#4F5266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M11.9955 13.7002H12.0045" stroke="#4F5266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M11.9955 16.7002H12.0045" stroke="#4F5266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M8.29431 13.7002H8.30329" stroke="#4F5266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M8.29431 16.7002H8.30329" stroke="#4F5266" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          <span>13:00 - 19:00, Oct 12 - 2023</span></p>
+          
+          <p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M2.29004 7.77998V17.51C2.29004 19.41 3.64004 20.19 5.28004 19.25L7.63004 17.91C8.14004 17.62 8.99004 17.59 9.52004 17.86L14.77 20.49C15.3 20.75 16.15 20.73 16.66 20.44L20.99 17.96C21.54 17.64 22 16.86 22 16.22V6.48998C22 4.58998 20.65 3.80998 19.01 4.74998L16.66 6.08998C16.15 6.37998 15.3 6.40998 14.77 6.13998L9.52004 3.51998C8.99004 3.25998 8.14004 3.27998 7.63004 3.56998L3.30004 6.04998C2.74004 6.36998 2.29004 7.14998 2.29004 7.77998Z" stroke="#4F5266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M8.56006 4V17" stroke="#4F5266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M15.73 6.62012V20.0001" stroke="#4F5266" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            <span>West Point St., Birmingham Dtr.</span></p>
+      </div>
     </div>
-  </nav>
 
-  
-
+  </section>
 </main>
+
+<style>
+
+  h2, p {
+    color: white;
+  }
+
+  h2 {
+    font-size: 4em;
+    margin: 0;
+    margin-bottom: 50px;
+  }
+
+  p {
+    color: #999DBA;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 32px;
+    margin: 0;
+    width: 376px;
+  }
+
+  .para {
+    margin-bottom: 124px;
+  }
+
+  .dates > p {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    margin-bottom: 24px;
+  }
+  main {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    min-height: 100vh;
+    height: 911px;
+    width: 100%;
+    background: #0B0C10;
+    overflow: hidden;
+  }
+
+  main > * {
+    z-index: 1;
+  }
+
+  section {
+    width: 100%;
+  }
+
+.about {
+  padding-top: 114px;
+  padding-left: 82.72px;
+  border-radius: 108px 0 0 0;
+background: #0B0C10;
+height: 100%;
+width: 100%;
+}
+
+  .about-bg {
+    position: absolute;
+    top: 0;
+    left: 15px;
+    z-index: 0;
+    height: 900px;
+  }
+
+
+  nav {
+    display: flex;
+    gap: 18px;
+    align-items: center;
+    padding: 30px 70px;
+    margin-bottom: 140px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .item {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+
+  .item > a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #2B2D3A;
+    padding: 12px;
+    border-radius: 50%;
+  }
+
+  .item-logo {
+    border-radius: 32px;
+    background: #000;
+    padding: 25px 48px 25px 30px;
+    border-radius: 32px;
+
+  }
+
+  .logo-con {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
+
+
+
+</style>
